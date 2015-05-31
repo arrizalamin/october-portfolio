25940b4a65b1739c5ce6f4f73a708835ae1064b8
Arrizal Amin <arrizalamin@gmail.com>
2015-05-31 09:05:00 +0700

    add support for Rainlab.Translate

--- models/Item.php
+++ models/Item.php
@@ -14,6 +14,11 @@ class Item extends Model
     public $table = 'arrizalamin_portfolio_items';
 
     /**
+     * @var array Translatable fields
+     */
+    public $translatable = ['title', 'description'];
+
+    /**
      * @var array Relations
      */
     public $belongsTo = [
@@ -22,5 +27,27 @@ class Item extends Model
     public $attachMany = [
         'images' => ['System\Models\File']
     ];
+
+     /**
+     * Add translation support to this model, if available.
+     * @return void
+     */
+    public static function boot()
+    {
+        // Call default functionality (required)
+        parent::boot();
+
+        // Check the translate plugin is installed
+        if (!class_exists('RainLab\Translate\Behaviors\TranslatableModel'))
+            return;
+
+        // Extend the constructor of the model
+        self::extend(function($model){
+
+            // Implement the translatable behavior
+            $model->implement[] = 'RainLab.Translate.Behaviors.TranslatableModel';
+
+        });
+    }
 
 }
