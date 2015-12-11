185feb4148ba8c807a89cdf59f810ba53c8c35e7
Arrizal Amin <arrizalamin@gmail.com>
2015-12-11 14:19:37 +0700

    delete seed_all_data.php and delete item 2 and 3

--- updates/seed_example_data.php
+++ updates/seed_example_data.php
@@ -11,12 +11,12 @@ class SeedExampleData extends Seeder
     public function run()
     {
         // exit when items are found
-        if(count(Item::all()) > 0) {
+        if(Item::count()) {
             return false;
         }
-        
+
         /**
-         * Add example categories
+         * Add example category
          */
         $ec = Category::create([
             'name' => 'Examples',
@@ -24,20 +24,14 @@ class SeedExampleData extends Seeder
         ]);
 
         /**
-         * Add example tags
+         * Add example tag
          */
         $t1 = Tag::create([
             'name' => 'one',
         ]);
-        $t2 = Tag::create([
-            'name' => 'two',
-        ]);
-        $t3 = Tag::create([
-            'name' => 'three',
-        ]);
 
         /**
-         * Add example items
+         * Add example item
          */
         $i1 = Item::create([
             'category_id' => $ec->id,
@@ -47,24 +41,6 @@ class SeedExampleData extends Seeder
         ]);
 
         $i1->tags()->attach($t1->id);
-
-        $i2 = Item::create([
-            'category_id' => $ec->id,
-            'title' => 'Intrinsicly integrate',
-            'slug' => 'intrinsicly',
-            'description' => 'Intrinsicly integrate future-proof e-tailers whereas high-quality users. Conveniently procrastinate world-class e-business vis-a-vis stand-alone technology. Competently streamline bleeding-edge process improvements after user friendly e-business. Dynamically myocardinate sticky results and visionary schemas. Assertively streamline standardized materials with distributed total linkage. Distinctively envisioneer multimedia based mindshare via extensive core competencies. Conveniently deliver market positioning initiatives before sustainable convergence. Collaboratively myocardinate stand-alone results for exceptional best practices. Completely initiate global mindshare with frictionless ideas. Synergistically reintermediate wireless intellectual capital rather than reliable.',
-        ]);
-
-        $i2->tags()->attach([$t1->id, $t2->id]);
-
-        $i3 = Item::create([
-            'category_id' => $ec->id,
-            'title' => 'Collaboratively initiate',
-            'slug' => 'collaboratively',
-            'description' => 'Collaboratively initiate top-line catalysts for change via process-centric growth strategies. Monotonectally leverage existing functional initiatives via enabled process improvements. Synergistically benchmark synergistic supply chains and economically sound supply chains. Assertively productivate front-end outsourcing and next-generation experiences. Progressively myocardinate intuitive scenarios with optimal e-services. Holisticly initiate pandemic platforms via granular human capital. Monotonectally promote diverse web-readiness without competitive niche markets. Enthusiastically evisculate enterprise-wide customer service with business vortals. Synergistically cultivate ethical information with bleeding-edge products. Globally evisculate functional networks.',
-        ]);
-
-        $i3->tags()->attach([$t1->id, $t2->id, $t3->id]);
 
         //TODO: find a way to attach sample images to the items.
     }
